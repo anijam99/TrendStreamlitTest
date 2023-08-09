c72fbc35f73abc35091903f72a3ef263a7152a0e
Tran Huy Minh (Jamie) <114696635+anijam99@users.noreply.github.com>
2023-08-10 05:26:11 +0800

    Update streamlit_app_group3.py

--- streamlit_app_group3.py
+++ streamlit_app_group3.py
@@ -1246,10 +1246,14 @@ with tabs[0]: #Tran Huy Minh S10223485H Tab Revenue Forecasting & Model Performa
                 mae = mean_absolute_error(y_true, y_pred)
                 mse = mean_squared_error(y_true, y_pred)
                 rmse = mean_squared_error(y_true, y_pred, squared=False)
+                '''1'''
                 if selected_model == 'Minh Model':
                     r2 = r2_score(np.expm1(y_true), np.expm1(y_pred))
+                    '''3'''
                 else:
                     r2 = r2_score(y_true, y_pred)
+                    '''4'''
+                '''2'''
     
                 # Display the performance metrics
                 st.subheader('Model Performance on Holdout data')
